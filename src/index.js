c7e6fa1c6545b5136940800473e59c755d59fac9
Andrew Rosário <andrew.arosario@gmail.com>
2018-04-05 12:32:24 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import {BrowserRouter as Router,Route,Switch,Link} from 'react-router-dom';
+import AutorBox from './Autor';
+import LivroBox from './Livro';
+import Home from './Home';
+
+ReactDOM.render(
+  (<Router>
+      <App>
+        <Switch>
+          <Route exact path="/" component={Home}/>
+          <Route path="/autor" component={AutorBox}/>
+          <Route path="/livro" component={LivroBox}/>
+        </Switch>
+      </App>
+  </Router>),
+  document.getElementById('root')
+);
